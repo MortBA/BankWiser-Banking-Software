a8a5e0160e7744b6f6bfd0976c2e0049561e23e7
dovhun <dovhun@student.chalmers.se>
2021-12-07 15:40:41 +0100

    EF1 Tests done

--- src/main/java/com/logic/bankwiser/facade/Facade.java
+++ src/main/java/com/logic/bankwiser/facade/Facade.java
@@ -1,4 +1,12 @@
 package com.logic.bankwiser.facade;
 
 public class Facade {
+    public String userLogin(String username, String password) {
+        return "";
+    }
+
+    public String createUserAccount(String username, String fullName, String password, String confirmPwd,
+                                    String phoneNumber, String userAddress, String socialSecurityNumber) {
+        return "";
+    }
 }
