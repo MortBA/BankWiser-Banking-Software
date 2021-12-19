dacba745ed12515eb1ee05724e01c78363f6d43e
Kevin Collins <kevinco@student.chalmers.se>
2021-12-20 00:54:04 +0100

    Resolving CardController conflicts

--- src/main/java/com/logic/bankwiser/controllers/CardController.java
+++ src/main/java/com/logic/bankwiser/controllers/CardController.java
@@ -176,22 +176,21 @@ public class CardController {
 
     public String remainderDays(String cardNumber) { //Calculates remaining days until expiration
         String address = "";
-        Date dateDate = new Date();
-        SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
-        String currentDate = date.format(dateDate);
-        int day = Integer.parseInt(currentDate.substring(8,10));
-        int month = Integer.parseInt(currentDate.substring(5,7));
-        int year = Integer.parseInt(currentDate.substring(0,4));
-
-        String expirationDate = STORAGE.getCard(cardNumber).getExpirationDate();
-        int expirationDay = Integer.parseInt(expirationDate.substring(8,10));
-        int expirationMonth = Integer.parseInt(expirationDate.substring(5,7));
-        int expirationYear = Integer.parseInt(expirationDate.substring(0,4));
-
-        int remainderYear = (expirationYear-year)*12*30;
-
-        int remainderDay = ((remainderYear+(expirationMonth*30)+expirationDay)-((month*30)+day));
-
+//        Date dateDate = new Date();
+//        SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
+//        String currentDate = date.format(dateDate);
+//        int day = Integer.parseInt(currentDate.substring(8,10));
+//        int month = Integer.parseInt(currentDate.substring(5,7));
+//        int year = Integer.parseInt(currentDate.substring(0,4));
+//
+//        String expirationDate = STORAGE.getCard(cardNumber).getExpirationDate();
+//        int expirationDay = Integer.parseInt(expirationDate.substring(8,10));
+//        int expirationMonth = Integer.parseInt(expirationDate.substring(5,7));
+//        int expirationYear = Integer.parseInt(expirationDate.substring(0,4));
+//
+//        int remainderYear = (expirationYear-year)*12*30;
+//
+//        int remainderDay = ((remainderYear+(expirationMonth*30)+expirationDay)-((month*30)+day));
 
         /*
             After some research, I see that Date is no longer really used in Java.
@@ -199,15 +198,21 @@ public class CardController {
             The implementation below does not mean this method is finished; but rather,
             The three lines below replace the 15 lines above the comment.
             Hopefully this is a good starting point! -KC
-           */
+         */
         LocalDate expirationDate = STORAGE.getCard(cardNumber).getExpirationDate();
         LocalDate dateToday = LocalDate.now();
         long remainderDays = ChronoUnit.DAYS.between(dateToday, expirationDate);
 
-        if(remainderDay<0){
+//        if(remainderDay<0){
+//            return "The card "+cardNumber+" expired and was terminated.";
+//        }else {
+//            return "Your card"+ cardNumber+" will expire in "+remainderDay+" days and will be terminated then. We have sent you a new one to "+address+".";
+//        }
+
+        if(remainderDays<0){
             return "The card "+cardNumber+" expired and was terminated.";
         }else {
-            return "Your card"+ cardNumber+" will expire in "+remainderDay+" days and will be terminated then. We have sent you a new one to "+address+".";
+            return "Your card"+ cardNumber+" will expire in "+remainderDays+" days and will be terminated then. We have sent you a new one to "+address+".";
         }
     }
 
