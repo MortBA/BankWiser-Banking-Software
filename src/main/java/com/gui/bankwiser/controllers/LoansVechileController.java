a2c2ed16c0f6580e92b31367f51c7686bf3365c4
Chanisra Magnusson <guschanima@student.gu.se>
2022-01-06 00:29:58 +0100

    Added Loans screens and controllers, changed employee screens

---
+++ src/main/java/com/gui/bankwiser/controllers/LoansVechileController.java
@@ -0,0 +1,75 @@
+package com.gui.bankwiser.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class LoansVechileController {
+    public void onLogOutClicked(ActionEvent event) {
+    }
+
+    public void onOverviewClicked(ActionEvent event) {
+    }
+
+    public void onAccountSettingsClicked(ActionEvent event) {
+    }
+
+    public void onMyCardsClicked(ActionEvent event) {
+    }
+
+    public void onTransferMoneyClicked(ActionEvent event) {
+    }
+
+    public void onTransactionHistoryClicked(ActionEvent event) {
+    }
+
+    public void onDeleteUserAccountClicked(ActionEvent event) {
+    }
+
+    public void onApplyPersonalLoanClicked(MouseEvent mouseEvent) {
+    }
+
+    public void onCreditCardClicked(MouseEvent mouseEvent) {
+    }
+
+    public void creditCardHoverIn(MouseEvent mouseEvent) {
+    }
+
+    public void creditCardHoverOut(MouseEvent mouseEvent) {
+    }
+
+    public void onApplyHomeLoanClicked(MouseEvent mouseEvent) {
+    }
+
+    public void onChangePinClicked(MouseEvent mouseEvent) {
+    }
+
+    public void changePinHoverIn(MouseEvent mouseEvent) {
+    }
+
+    public void changePinHoverOut(MouseEvent mouseEvent) {
+    }
+
+    public void onOverviewLoanClicked(MouseEvent mouseEvent) {
+    }
+
+    public void onDebitCardClicked(MouseEvent mouseEvent) {
+    }
+
+    public void debitCardHoverIn(MouseEvent mouseEvent) {
+    }
+
+    public void debitCardHoverOut(MouseEvent mouseEvent) {
+    }
+
+    public void onApplyVechileLoanClicked(MouseEvent mouseEvent) {
+    }
+
+    public void onModifyFuncClicked(MouseEvent mouseEvent) {
+    }
+
+    public void cardFuncHoverIn(MouseEvent mouseEvent) {
+    }
+
+    public void cardFuncHoverOut(MouseEvent mouseEvent) {
+    }
+}
