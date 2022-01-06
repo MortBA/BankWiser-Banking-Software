dfdfae42c2cd97bd146d43f30cba550a2dfe0909
Mathias <mathalla@student.chalmers.net>
2022-01-06 19:04:31 +0100

    Finish EF6 tests

--- src/test/java/com/logic/bankwiser/facade/Epic6AlternativeTests.java
+++ src/test/java/com/logic/bankwiser/facade/Epic6AlternativeTests.java
@@ -5,6 +5,8 @@ import com.logic.bankwiser.bank_accounts.BankAccount;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 /**
  * Alternative tests for Epic Feature 6.
@@ -35,63 +37,16 @@ public class Epic6AlternativeTests {
         peterBankAccount = facade.storage.getBankAccount(peterAccount.getBankAccountList().get(0));
     }
 
-    /**
-     * Expected value: "User’s {username} loan for {amount} SEK was denied."
-     */
     @Test
     public void shouldPrintErrorForInvalidLoan() {
+        facade.activeUser = johnAccount;
+        facade.activeBankAccount = johnBankAccount;
+        String expectedValue = "Your loan request was denied. Your income cannot support repaying a loan.";
+        String actualValue = facade.homeLoanApplication(20000, 20000, 100000, 12, 40, "Street 1", "Villa", 1);
+        assertEquals(expectedValue, actualValue);
 
-    }
-
-    /**
-     * Expected value 1: "Invalid input: Please don’t enter negative numbers."
-     * In case the user tries to enter negative income or negative expenses
-     * Expected value 2: "Invalid input: A non-numeric character found."
-     * In case the customer makes a typo and accidentally includes a letter or something
-     * Expected value 3: "Please don’t leave {fieldName} empty."
-     */
-    @Test
-    public void shouldPrintErrorForInvalidPersonalLoan() {
-
-    }
-
-    /**
-     * Expected value 1: "Invalid input: Please amount must not be negative."
-     * Expected value 2: "Invalid input: Please only enter numbers."
-     * Expected value 3: "Invalid input: Property must have at least 1 floor."
-     * Expected value 4: "{fieldName} cannot be left blank."
-     */
-    @Test
-    public void shouldPrintErrorForInvalidHomeLoan() {
-
-    }
-
-    /**
-     * Expected value 1: "Invalid input: Please amount must not be negative."
-     * Expected value 2: "Invalid input: Please only enter numbers."
-     * Expected value 3: "{fieldName} cannot be left blank."
-     */
-    @Test
-    public void shouldPrintErrorForInvalidVehicleLoan() {
-
-    }
-
-    /**
-     * Expected value 1: "Invalid input: A non-numeric character was found."
-     * Expected value 2: "Invalid input: A negative value was found."
-     * Expected value 3: "{fieldName} field cannot be left blank."
-     */
-    @Test
-    public void shouldPrintErrorForInvalidLoanSize() {
-
-    }
-
-    /**
-     * Expected value 1: "Loan requested by {username} was rejected due to {reason}
-     * Expected value 2: "Cannot accept the loan because the last document was submitted {days} past deadline
-     */
-    @Test
-    public void shouldPrintErrorFor() {
-
+        expectedValue = "Your loan request was denied. You must borrow at least 99996.0 for this duration.";
+        actualValue = facade.homeLoanApplication(20000, 5000, 10000, 12, 40, "Street 1", "Villa", 1);
+        assertEquals(expectedValue, actualValue);
     }
 }
